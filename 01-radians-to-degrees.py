fb54163e2c7fef517d6f3075b690c069ab5a291e
SPlettAltow <steven.plett@altow.de>
2022-06-16 11:18:55 +0200

    Aufg. 1 gelöst

--- 01-radians-to-degrees.py
+++ 01-radians-to-degrees.py
@@ -1,18 +1,14 @@
 ##################################################################
 # Exercise 1: Radians to Degrees
 # Write a function that converts an angle in radians to degrees.
-
+import math
 def radiansToDegrees(radians):
-    # WRITE CODE HERE
-    pass
+    return (180/math.pi)*radians
 
 ##################################################################
 def test_radiansToDegrees():
     assert radiansToDegrees(0) == 0
-    assert radiansToDegrees(0.5) == 0.927295
-    assert radiansToDegrees(1) == 1.5707963267948966
-    assert radiansToDegrees(1.5) == 2.6179938779914944
-    assert radiansToDegrees(2) == 3.141592653589793
+    assert radiansToDegrees(math.pi) == 180
 
 if __name__ == '__main__':
     test_radiansToDegrees()
