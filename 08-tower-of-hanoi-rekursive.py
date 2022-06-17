8916a9429d7f8c6f6697ad74144977ceb8731432
Alexander Tews <alexander.tews@altow.de>
2022-06-17 14:30:42 +0200

    neue Aufgabe

---
+++ 08-tower-of-hanoi-rekursive.py
@@ -0,0 +1,6 @@
+##################################################################
+# Exercise 9: Tower of Hanoi
+# Write a program to move the disks from source rod to destination rod using spare rod.
+
+def tower(n, source, dest, spare):
+    pass
