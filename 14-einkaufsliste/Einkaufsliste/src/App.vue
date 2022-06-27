70121941d246d29d3ad38662b10be1500ce9fce1
SPlettAltow <steven.plett@altow.de>
2022-06-27 16:17:50 +0200

    still todo

--- 14-einkaufsliste/Einkaufsliste/src/App.vue
+++ 14-einkaufsliste/Einkaufsliste/src/App.vue
@@ -44,53 +44,169 @@ const productsInList = ref([
     }
   }
 ])
+/*
+savedProductsInList[0] -> Name für die Einkauslisten
+savedProductsInList[1] -> Listen
+savedProductsInList[1][X] -> x = gruppen
+*/
+const savedProductsInList = [
+{
+  [0]: {
+    name: "TestList",
+    id: 0
+  },
+  [1]: {
+    name: "SecondTestLest",
+    id: 1
+  }
+},
+{
+  [0]: [
+    {
+    group: "Obst",
+    products: {
+      [1]: {
+        id: 1,
+        name: "1x Luft",
+        amount: 1,
+        isPurchased: false
+      },
+      [2]: {
+        id: 2,
+        name: "1x Gold",
+        amount: 1,
+        isPurchased: false
+      }
+    }
+  }
+  ],
+
+  [1]: [
+    {
+    group: "Obst",
+    products: {
+      [1]: {
+        id: 1,
+        name: "1x Test Luft",
+        amount: 1,
+        isPurchased: false
+      },
+      [2]: {
+        id: 2,
+        name: "1x Test Gold",
+        amount: 1,
+        isPurchased: false
+      }
+    }
+  }
+  ]
+}
+]
+
+
+// Objects.keys(savedProductsInList.value['Einkaufslistenname']['listvalue']) -> zugriff auf gespeicherte daten (gruppen & deren produkte)
 const selectedGroup = ""
 const productName = ""
-let isButtonDisabled=false;
-let listName = "Einkaufslistenname"
+const amount = 1
+let isButtonDisabled=ref(false);
+let listName = ref("Einkaufslistenname")
 
-function setListName(newListName: string) {
-  listName = newListName
-  isButtonDisabled = true
+function setListName(event: Event) {
+  event.preventDefault();
+  listName.value = event.target[0].value
+  isButtonDisabled.value = true;
 }
 
-function addProduct(productName: string, groupName: string) {
+function saveList(){
+  let newID = Object.keys(savedProductsInList[0]).length
+  let newListName = listName.value
+  let listvalue = productsInList.value
+  savedProductsInList[0][newID] = {
+    name: newListName,
+    id: newID
+  }
 
+  savedProductsInList[1][newID] = productsInList.value
+}
+
+function loadList(id: string) {
+  productsInList.value = savedProductsInList[1][id]
+  listName.value = savedProductsInList[0][id].name
+  console.log("called")
+}
+
+function deleteList(id: string) {
+  if (!savedProductsInList[1][id]) return;
+  delete savedProductsInList[1][id]
+  productsInList.value = []
+  listName.value = "Einkaufslistenname"
+}
+
+function conditionlessAddProduct(existsAlready: boolean, groupIndex: number, indexedProducts: string, productName: string, id: number, latestID: number, amount: number) {
+  if(existsAlready === false) {
+    productsInList.value[groupIndex].products[latestID] = {
+      id: latestID,
+      name: amount + "x " + productName,
+      amount: amount,
+      isPurchased: false
+    }
+  } else {
+    let product = productsInList.value[groupIndex].products[parseInt(id)]
+    console.log(product)
+    product.amount += amount
+    product.name = product.amount + "x " + productName
+  }
+
+  saveList()
+}
+
+function addProduct(productName: string, groupName: string, amount: number) {
+
+  let found = false;
+  let groupIndexP = 0;
+  let indexedProductsP = "";
+  let productNameP = "";
+  let latestID = 0;
+  let productIdP = 0;
+  let amountP = 0;
   productsInList.value.map((groups, groupIndex) => {
 
     if (groups.group == groupName) {
-
       Object.keys(groups.products).map((indexedProducts, productIndex) => {
-        console.log("_","indexedProducts",indexedProducts,"productIndex",productIndex,"groups",groups,"groupIndex",groupIndex)
-        console.log("ran")
         let theProduct = groups.products[parseInt(indexedProducts)]
+
         if (groups.products[parseInt(indexedProducts)].name.includes(productName)) {
-          theProduct.amount += 1
-          theProduct.name = theProduct.amount + "x " + productName
-        } else {
-          const latestID = Object.keys(groups.products).length + 1
-          groups.products[latestID] = {
-            id: latestID,
-            name: "1x " + productName,
-            amount: 1,
-            isPurchased: false
-          }
+          console.log("found")
+          found = true;
+          productIdP = productIndex
         }
-        console.log("finished")
+        groupIndexP = groupIndex
+        indexedProductsP = indexedProducts
+        productNameP = productName
+        amountP = amount
+        latestID = Object.keys(groups.products).length + 1
       })
     }
   })
+  console.log(found, groupIndexP, indexedProductsP, productNameP, productIdP, latestID,amountP) 
+  conditionlessAddProduct(found, groupIndexP, indexedProductsP, productNameP, productIdP+1, latestID, amountP)
 }
 
 function removeProduct(groupName: string,id: number) {
 productsInList.value.forEach((indexedGroups) => {
   if (indexedGroups.group == groupName) {
+    let product = indexedGroups.products[id]
+    if(product.amount > 1) {
+      product.amount -= 1
+      let newname = product.name.split('x ').pop()
+      console.log(newname)
+      product.name = product.amount + "x " + newname
+    } else {
     delete indexedGroups.products[id];
-    console.log(indexedGroups.group, Object.keys(indexedGroups.products).length)
+    }
   }
 }
 )
-  console.log(groupName, id)
 }
 
 </script>
@@ -100,14 +216,16 @@ productsInList.value.forEach((indexedGroups) => {
 
   <div class="templateGroupA">
     <div class="setShoppingListName defaultBg">
-      <input type="text" id="shoppingListName" v-model="listName" placeholder="Einkaufslistenname">
-      <button id="setNameButton" class="defaultButton" @click="setListName(listName)">Festlegen</button>
+      <form class="shoppingListForm" @submit="setListName">
+        <input type="text" id="shoppingListName" placeholder="Einkaufslistenname">
+        <button :disabled="isButtonDisabled" id="setNameButton" class="defaultButton" type="submit">Festlegen</button>
+      </form>
     </div>
 
     <div class="addProduct defaultBg">
       <h2>Produkt hinzufügen</h2>
       <div class="addProductGroup">
-        <input type="number" id="amount" placeholder="0">
+        <input type="number" id="amount" v-model="amount" placeholder="1">
         <input type="text" id="productName" v-model="productName" placeholder="Produktname eingeben">
         <select id="groups" v-model="selectedGroup">
           <option disabled value="">Gruppe</option>
@@ -116,18 +234,27 @@ productsInList.value.forEach((indexedGroups) => {
         </select>
       </div>
 
-      <button id="addProductButton" class="defaultButton" @click="addProduct(productName, selectedGroup)">Produkt hinzufügen</button>
+      <button :disabled="!isButtonDisabled" id="addProductButton" class="defaultButton" @click="addProduct(productName, selectedGroup, amount)">Produkt hinzufügen</button>
     </div>
 
     <div class="addedProductsGroup defaultBg">
-      <h2>Placeholder</h2>
+      <h2>{{ listName }}</h2>
       <div class="addedProducts">
         <div v-for="products in productsInList" :key="products.group" class="addedProductGroups">
           <h3 v-if="Object.keys(products.products).length > 0">{{ products.group }}</h3>
           <p v-for="allProducts in products.products" :key="allProducts.valueOf().id" :class="allProducts.valueOf().isPurchased ? 'productPurchased' : 'product'" @click="removeProduct(products.group,allProducts.valueOf().id)">{{allProducts.valueOf().name}}</p>
         </div>
       </div>
+      <div class="containerB">
+        <button id="saveList" class="defaultButton" @click="saveList">Speichern</button>
+        <button id="deleteList" class="defaultButton" @click="deleteList(listnames.id)">Löschen</button>
+      </div>
+    </div>
 
+    <div class="savedListsGroup defaultBg">
+      <div v-for="lists in savedProductsInList" :key="lists" class="savedLists">
+        <h3 v-for="listnames in lists" @click="loadList(listnames.id)">{{ listnames.name }}</h3>
+      </div>
     </div>
 
   </div>
@@ -189,6 +316,13 @@ productsInList.value.forEach((indexedGroups) => {
     height: 108px;
   }
 
+  .setShoppingListName .shoppingListForm {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
   .setShoppingListName #shoppingListName {
     text-align: center;
     background-color: #D9D9D9;
@@ -210,11 +344,11 @@ productsInList.value.forEach((indexedGroups) => {
     transition: 0.1s;
   }
 
-  .setShoppingListName #setNameButton:hover {
+  .setShoppingListName #setNameButton:hover:enabled {
     background-color: #0ecf21;
   }
 
-  .setShoppingListName #setNameButton:active {
+  .setShoppingListName #setNameButton:active:enabled {
     transform: scale(0.98);
   }
 
@@ -290,19 +424,70 @@ productsInList.value.forEach((indexedGroups) => {
     width: 340px;
   }
 
-  .addProduct #addProductButton:hover {
+  .addProduct #addProductButton:disabled {
+    background-color: #c9c9c9;
+  }
+
+  .addProduct #addProductButton:hover:enabled {
     background-color: #0ecf21;
     transition: 0.1s;
   }
 
-  .addProduct #addProductButton:active {
+  .addProduct #addProductButton:active:enabled {
     transform: scale(0.98);
   }
 
   .addedProductsGroup {
+    justify-content: space-between;
+    flex-direction: column;
+    display: flex;
     height: 460px;
     width: 750px;
   }
+
+  .addedProductsGroup h2 {
+      border-bottom-style: solid;
+      padding-left: 10px;
+  }
+
+  .addedProductsGroup #saveList {
+    color:#F9F9F9;
+    background-color: #3873d1;
+    float: right;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    height: 25px;
+    width: 100px;
+  }
+
+
+  .addedProductsGroup #saveList:hover:enabled {
+    background-color: #4287f5;
+    transition: 0.1s;
+  }
+
+  .addedProductsGroup #saveList:active:enabled {
+    transform: scale(0.98)
+  }
+
+  .addedProductsGroup #deleteList {
+    background-color: #fa3455;
+    color: #F9F9F9;
+    float: right;
+    margin-right: 10px;
+    height: 25px;
+    width: 100px;
+  }
+
+  .addedProductsGroup #deleteList:hover {
+    background-color: #d1304b;
+    transition: 0.1s;
+  }
+
+  .addedProductsGroup #deleteList:active {
+    transform: scale(0.98)
+  }
+
 
   .addedProducts {
     display: flex;
